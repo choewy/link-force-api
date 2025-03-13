813d1b29c772fb20b3ef6756bc1f0ad631051595
choewy <choewy32@gmail.com>
2025-03-13 23:03:01 +0900

    chore: comment

--- src/application/sign/sign.controller.ts
+++ src/application/sign/sign.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, HttpStatus, Post, Query, Res } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { Response } from 'express';
 import * as qs from 'qs';
@@ -13,6 +13,7 @@ import { ServiceTokenResponseDTO } from './dto/service-token-response.dto';
 import { ServiceTokenRequestDTO } from './dto/service-token-request.dto';
 import { KakaoLoginPageRequestDTO } from './dto/kakao-login-page-request.dto';
 
+@ApiTags('인증')
 @Controller('sign')
 export class SignController {
   constructor(
@@ -21,6 +22,7 @@ export class SignController {
     private readonly signService: SignService,
   ) {}
 
+  // FIXME
   @Get('kakao')
   @ApiOperation({ summary: '카카오 로그인 페이지 진입' })
   getKakaoLoginPageURL(@Query() query: KakaoLoginPageRequestDTO, @Res() response: Response): void {
@@ -29,8 +31,9 @@ export class SignController {
     response.redirect(HttpStatus.MOVED_PERMANENTLY, url);
   }
 
+  // FIXME
   @Get('kakao/callback')
-  @ApiOperation({ summary: '카카오 로그인 Callback', deprecated: true })
+  @ApiOperation({ summary: '카카오 로그인 Callback' })
   async kakaoLoginCallback(@Query() queryParam: KakaoLoginCallbackRequestDTO, @Res() response: Response) {
     const code = await this.signService.signInWithKakao(queryParam.code);
     const url = [queryParam.state, qs.stringify({ code })].join('?');
@@ -38,6 +41,7 @@ export class SignController {
     response.redirect(HttpStatus.MOVED_PERMANENTLY, url);
   }
 
+  // FIXME
   @Post('token')
   @ApiOperation({ summary: '서비스 토큰 발급' })
   @ApiCreatedResponse({ type: ServiceTokenResponseDTO })
