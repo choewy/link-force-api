ffc59ee621b6fc67ce31939aaa78853815f1f560
choewy <choewy32@gmail.com>
2025-03-13 21:06:35 +0900

    chore: .husky

--- src/application/auth/dto/kakao-login-page-request.dto.ts
+++ src/application/auth/dto/kakao-login-page-request.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class KakaoLoginPageRequestDTO {
   @ApiProperty({ type: String })
